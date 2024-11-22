999400000c5560bf187055868ad4fb9c919210fb
Ramez Rafla <ramez@classroomapp.com>
2024-11-22 11:55:29 -0500

    fixes to model/train_step

--- scripts/model.py
+++ scripts/model.py
@@ -27,7 +27,7 @@ class GPT(keras.Model):
         self.attention_mask = np.tril(np.ones((context_size, context_size)), 0)
         self.dropout = dropout
         self.vocab_size = vocab_size
-        self.metrics = ['accuracy']
+        self.metrics = [keras.metrics.Accuracy()]
         
         self.token_embed = layers.Embedding(
             input_dim = self.vocab_size, 
@@ -87,14 +87,28 @@ class GPT(keras.Model):
         x = self.final_dense(x)
         return x
 
+    # https://www.tensorflow.org/guide/keras/customizing_what_happens_in_fit
     def train_step(self, data):
-        with tf.GradientTape() as tape:
-            inputs = data[0]
-            labels = data[1]
-            probs = self(inputs, training = True)
-            loss = self.compute_loss(x=inputs, y=labels, y_pred=probs)
-            gradients = tape.gradient(loss, self.trainable_variables)
-            self.optimizer.apply_gradients(zip(gradients, self.trainable_variables))
-            # self.compute_metrics(x=inputs, y=labels, y_pred=probs)
+        x, y = data
 
+        with tf.GradientTape() as tape:
+            y_pred = self(x, training = True) # Forward pass
+            # Compute the loss value
+            # (the loss function is configured in `compile()`)
+            loss = self.compute_loss(y=y, y_pred=y_pred)
+
+        # Compute gradients
+        trainable_vars = self.trainable_variables
+        gradients = tape.gradient(loss, trainable_vars)
+        # Update weights
+        self.optimizer.apply_gradients(zip(gradients, trainable_vars))
+
+        # Update metrics (includes the metric that tracks the loss)
+        for metric in self.metrics:
+            if metric.name == "loss":
+                metric.update_state(loss)
+            else:
+                metric.update_state(y, y_pred)
+
+        # Return a dict mapping metric names to current value
         return { m.name: m.result() for m in self.metrics }
